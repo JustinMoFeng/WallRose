b8015689e1ed387204998a9b4d8bf96eaacb3d29
mofengyichen <3309113612@qq.com>
2023-12-21 00:41:40 +0800

    手搓界面ing

--- app/src/main/java/com/shingekinokyojin/wallrose/services/FloatingWindowService.kt
+++ app/src/main/java/com/shingekinokyojin/wallrose/services/FloatingWindowService.kt
@@ -4,16 +4,31 @@ import android.app.Service
 import android.content.Intent
 import android.graphics.PixelFormat
 import android.os.IBinder
+import android.view.View
 import android.view.WindowManager
+import androidx.compose.ui.platform.ComposeView
+import com.shingekinokyojin.wallrose.ui.composables.floating.MyFloatingContent
 
 class FloatingWindowService : Service() {
+
+    private lateinit var windowManager: WindowManager
+    private lateinit var floatingView: View
+
     override fun onBind(intent: Intent?): IBinder? {
         return null
     }
 
     override fun onCreate() {
         super.onCreate()
-        val windowManager = getSystemService(WINDOW_SERVICE) as WindowManager
+
+        windowManager = getSystemService(WINDOW_SERVICE) as WindowManager
+
+        floatingView = ComposeView(this).apply {
+            setContent {
+                MyFloatingContent()
+            }
+        }
+
         val layoutParams = WindowManager.LayoutParams().apply {
             type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
             flags = 0
@@ -21,6 +36,8 @@ class FloatingWindowService : Service() {
             width = WindowManager.LayoutParams.WRAP_CONTENT
             height = WindowManager.LayoutParams.WRAP_CONTENT
         }
+
+        windowManager.addView(floatingView, layoutParams)
     }
 
     override fun onDestroy() {
---
+++ app/src/main/java/com/shingekinokyojin/wallrose/ui/composables/floating/FloatingWindow.kt
@@ -0,0 +1,9 @@
+package com.shingekinokyojin.wallrose.ui.composables.floating
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MyFloatingContent() {
+    Text(text = "Floating Window")
+}
