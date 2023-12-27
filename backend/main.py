79c6597de6e6f9348e9f77a664abd0a576254537
ALiersEL <johnathansteinfeld@gmail.com>
2023-12-28 01:00:26 +0800

    feat: Implemented user authentication functionality with database integration

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,5 @@
 from fastapi import Depends, FastAPI
 from routers import users,gpt_streaming_api,login
-from dependencies.path_dependencies import get_query_token, verify_token
 
 from dotenv import load_dotenv
 load_dotenv()
--- backend/models/user_models.py
+++ backend/models/user_models.py
@@ -1,13 +1,40 @@
-from pydantic import BaseModel
-from typing import Optional, Union
+from pydantic import BaseModel, Field
+from typing import Optional, Union, Any
+from bson import ObjectId
+from pydantic_core import core_schema
 
+class PyObjectId(str):
+    @classmethod
+    def __get_pydantic_core_schema__(
+            cls, _source_type: Any, _handler: Any
+    ) -> core_schema.CoreSchema:
+        return core_schema.json_or_python_schema(
+            json_schema=core_schema.str_schema(),
+            python_schema=core_schema.union_schema([
+                core_schema.is_instance_schema(ObjectId),
+                core_schema.chain_schema([
+                    core_schema.str_schema(),
+                    core_schema.no_info_plain_validator_function(cls.validate),
+                ])
+            ]),
+            serialization=core_schema.plain_serializer_function_ser_schema(
+                lambda x: str(x)
+            ),
+        )
+
+    @classmethod
+    def validate(cls, value) -> ObjectId:
+        if not ObjectId.is_valid(value):
+            raise ValueError("Invalid ObjectId")
+
+        return ObjectId(value)
 
 class User(BaseModel):
-    id: int
     username: str
     password: Optional[str] = None
 
 class UserInDB(User):
+    user_id: PyObjectId | None = Field(alias="_id", default=None)
     hashed_password: str
 
 class Token(BaseModel):
@@ -17,6 +44,4 @@ class Token(BaseModel):
 
 class TokenData(BaseModel):
     username: Union[str, None] = None
-    id: Union[int, None] = None
-
-fake_users_db = {}
+    user_id: Union[int, None] = None
